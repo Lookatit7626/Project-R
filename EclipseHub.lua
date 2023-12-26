c655e89b6cc0089f59809d700f36c02053c80b2d
Lookatit7626 <105156427+Lookatit7626@users.noreply.github.com>
2023-12-26 12:01:02 -0500

    Update EclipseHub.lua

--- EclipseHub.lua
+++ EclipseHub.lua
@@ -5,9 +5,9 @@ local GUI = Library.CreateGUI("Eclipse's HUB","rbxassetid://14497104058")
 
 local Section = Library.CreateSection(GUI,"Home")
 
-local button1 = Library.CreateButton(Section,"Name","Enforcing State Security GameWide")
+Library.CreateButton(Section,"Name","Enforcing State Security GameWide")
 
-local button2 = Library.CreateButton(Section,"Delete","Delete Hub",function()
+Library.CreateButton(Section,"Delete","Delete Hub",function()
 	Library.CreateNotification("Deleting","Deleting HUB...")
 	wait(2)
 	GUI:Destroy() 
@@ -15,18 +15,18 @@ end)
 
 local Section2 = Library.CreateSection(GUI,"UniversalScript")
 
-local button3 = Library.CreateButton(Section2,"Build A Boat","Build a Boat Farm",function() loadstring(game:HttpGet("https://pastebin.com/raw/h9AnD9d1"))() end)
+Library.CreateButton(Section2,"Build A Boat","Build a Boat Farm",function() loadstring(game:HttpGet("https://pastebin.com/raw/h9AnD9d1"))() end)
 
-local button3 = Library.CreateButton(Section2,"Bedwars","Bedwars",function() loadstring(game:HttpGet("https://raw.githubusercontent.com/AlSploit/AlSploit/main/Bedwars"))() end)
+Library.CreateButton(Section2,"Bedwars","Bedwars",function() loadstring(game:HttpGet("https://raw.githubusercontent.com/AlSploit/AlSploit/main/Bedwars"))() end)
 
-local button4 = Library.CreateButton(Section2,"Dex","IY Mobile Dex",function()
+Library.CreateButton(Section2,"Dex","IY Mobile Dex",function()
 	getgenv().Key = "Bash"
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Hosvile/Refinement/main/MC%3AIY%20Dex",true))() 
 end)
 
-local button5 = Library.CreateButton(Section2,"IY","IY",function() loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))() end)
+Library.CreateButton(Section2,"IY","IY",function() loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))() end)
 
-local button6 = Library.CreateButton(Section2,"Fling","Custom Fling Script",function() loadstring(game:HttpGet("https://pastebin.com/raw/WnJwFXgR"))() end)
+Library.CreateButton(Section2,"Fling","Custom Fling Script",function() loadstring(game:HttpGet("https://pastebin.com/raw/WnJwFXgR"))() end)
 
 local PlayerScript = Library.CreateSection(GUI,"Player Script")
 
@@ -93,7 +93,7 @@ Library.CreateTextBoxButton(PlayerScript,"Teleport",'Teleport',function(Playerna
 	else
 		Library.CreateNotification("Alert!", "Can't find the player you want to teleport!")
 	end
-	
+
 end,"Player name")
 
 Library.CreateLoopButton(PlayerScript,"AutoReport", "AutoReport", function()
@@ -135,86 +135,90 @@ Library.CreateLoopButton(PlayerScript,"AutoReport", "AutoReport", function()
 	end)()
 end,0.1)
 
+local Animations = Library.CreateSection(GUI,"Animations")
+
+Library.CreateButton(Animations,"R15-R6","R15-R6",function() loadstring(game:HttpGet('https://raw.githubusercontent.com/Lookatit7626/Project-R/main/R15-R6'))() end)
+
 local HeavenSword = Library.CreateSection(GUI,"Heaven Sword")
 
 local button = Library.CreateLoopButton(HeavenSword,"Kill All","Kill All (NPCS)",function()
-		local args = {
+	local args = {
 		[1] = "A7"
-		}
+	}
 
-		game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("Force"):FireServer(unpack(args))
+	game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("Force"):FireServer(unpack(args))
 
-		local args = {
-			[1] = "InvA10",
-			[2] = 1000,
-			[3] = "MP",
-			[4] = "Potion"
-		}
+	local args = {
+		[1] = "InvA10",
+		[2] = 1000,
+		[3] = "MP",
+		[4] = "Potion"
+	}
 
-		game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("UseItem"):FireServer(unpack(args))
+	game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("UseItem"):FireServer(unpack(args))
 end,0.05)
 local button = Library.CreateLoopButton(HeavenSword,"InfHP","Inf HP all",function()
 
-		local args = {
-			[1] = "InvA10",
-			[2] = 1000,
-			[3] = "HP",
-			[4] = "Potion"
-		}
+	local args = {
+		[1] = "InvA10",
+		[2] = 1000,
+		[3] = "HP",
+		[4] = "Potion"
+	}
 
-		game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("UseItem"):FireServer(unpack(args))
+	game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("UseItem"):FireServer(unpack(args))
 end,0.05)
 local button = Library.CreateLoopButton(HeavenSword,"HealAll","Heal All",function()
 
-		local args = {
+	local args = {
 		[1] = "A7"
-		}
+	}
 
-		game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("Cure"):FireServer(unpack(args))
+	game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("Cure"):FireServer(unpack(args))
 
-		local args = {
-			[1] = "InvA10",
-			[2] = 1000,
-			[3] = "MP",
-			[4] = "Potion"
-		}
+	local args = {
+		[1] = "InvA10",
+		[2] = 1000,
+		[3] = "MP",
+		[4] = "Potion"
+	}
 
-		game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("UseItem"):FireServer(unpack(args))
+	game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("UseItem"):FireServer(unpack(args))
 
 end,.05)
 local button = Library.CreateLoopButton(HeavenSword,"Immunity","Immunity",function()
 
-		local args = {
+	local args = {
 		[1] = "A7"
-		}
+	}
 
-		game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("Barrier"):FireServer(unpack(args))
+	game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("Barrier"):FireServer(unpack(args))
 
-		local args = {
-			[1] = "InvA10",
-			[2] = 1000,
-			[3] = "MP",
-			[4] = "Potion"
-		}
+	local args = {
+		[1] = "InvA10",
+		[2] = 1000,
+		[3] = "MP",
+		[4] = "Potion"
+	}
 
-		game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("UseItem"):FireServer(unpack(args))
+	game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("UseItem"):FireServer(unpack(args))
 
 end,.05)
 local button = Library.CreateLoopButton(HeavenSword,"Fireball","Fireball",function()
 
-		local args = {
-			[1] = "C2"
-		}
+	local args = {
+		[1] = "C2"
+	}
 
-		game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("Fireball"):FireServer(unpack(args))
+	game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("Fireball"):FireServer(unpack(args))
 
-		local args = {
-			[1] = "InvA10",
-			[2] = 1000,
-			[3] = "MP",
-			[4] = "Potion"
-		}
+	local args = {
+		[1] = "InvA10",
+		[2] = 1000,
+		[3] = "MP",
+		[4] = "Potion"
+	}
 
-		game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("UseItem"):FireServer(unpack(args))
+	game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("UseItem"):FireServer(unpack(args))
 
 end,.05)
