e5463487dfc5a437103aa7197450506c85ffe94b
Lookatit7626 <105156427+Lookatit7626@users.noreply.github.com>
2024-01-28 15:04:44 -0500

    Update EclipseHub.lua

--- EclipseHub.lua
+++ EclipseHub.lua
@@ -781,6 +781,8 @@ end,0.001)
 
 local HeavenSword = Library.CreateSection(GUI,"Heaven Sword")
 
+local PS99 = Library.CreateSection(GUI,"PS99")
+
 button = Library.CreateLoopButton(HeavenSword,"Kill All","Kill All (NPCS)",function()
 	local args = {
 		[1] = "A7"
