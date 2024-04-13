29d9e40e86ac56b2a3f6c86c0daf178cb8708cde
Lookatit7626 <105156427+Lookatit7626@users.noreply.github.com>
2024-04-12 20:49:43 -0400

    Update EclipseHub.lua

--- EclipseHub.lua
+++ EclipseHub.lua
@@ -5,6 +5,15 @@ local Words = loadstring(game:HttpGet(('https://raw.githubusercontent.com/Lookat
 
 local GUI = Library.CreateGUI("Eclipse's HUB","rbxassetid://14943920709")
 
+
+--GLOBAL FUNCTIONS
+
+function round(num, numDecimalPlaces)
+	local mult = 10^(numDecimalPlaces or 0)
+	return math.floor(num * mult + 0.5) / mult
+end
+
+
 local Section = Library.CreateSection(GUI,"Home")
 
 Library.CreateButton(Section,"Name","Hello! "..game.Players.LocalPlayer.Name)
@@ -12,7 +21,7 @@ Library.CreateButton(Section,"Name","Hello! "..game.Players.LocalPlayer.Name)
 Library.CreateButton(Section,"CreditsSec","-----Credits-----")
 
 Library.CreateButton(Section,"Credits","Engineered by ECLIPSE and [Redacted] | Tested by Twilight")
-Library.CreateButton(Section,"NameGroup","Eclipse Software - V2.3")
+Library.CreateButton(Section,"NameGroup","Eclipse Software - V43.5D")
 
 Library.CreateButton(Section,"CreditsSec","-----Settings-----")
 
@@ -646,6 +655,134 @@ Library.CreateLoopButton(PlayerScript,"AutoReport", "AutoReport", function()
 		end
 	end)()
 end,0.1)
+
+local ESPenabled = false
+local cloneref = cloneref or function(o) return o end
+COREGUI = cloneref(game:GetService("CoreGui"))
+function ESP(plr)
+	task.spawn(function()
+		for i,v in pairs(COREGUI:GetChildren()) do
+			if v.Name == plr.Name..'_ESP' then
+				v:Destroy()
+			end
+		end
+		wait()
+		if plr.Character and plr.Name ~= game.Players.LocalPlayer.Name and not COREGUI:FindFirstChild(plr.Name..'_ESP') then
+			local ESPholder = Instance.new("Folder")
+			ESPholder.Name = plr.Name..'_ESP'
+			ESPholder.Parent = COREGUI
+			repeat wait(1) until plr.Character and getRoot(plr.Character) and plr.Character:FindFirstChildOfClass("Humanoid")
+			for b,n in pairs (plr.Character:GetChildren()) do
+				if (n:IsA("BasePart")) then
+					local a = Instance.new("BoxHandleAdornment")
+					a.Name = plr.Name
+					a.Parent = ESPholder
+					a.Adornee = n
+					a.AlwaysOnTop = true
+					a.ZIndex = 10
+					a.Size = n.Size
+					a.Transparency = 0.4
+					a.Color = plr.TeamColor
+				end
+			end
+			if plr.Character and plr.Character:FindFirstChild('Head') then
+				local BillboardGui = Instance.new("BillboardGui")
+				local TextLabel = Instance.new("TextLabel")
+				BillboardGui.Adornee = plr.Character.Head
+				BillboardGui.Name = plr.Name
+				BillboardGui.Parent = ESPholder
+				BillboardGui.Size = UDim2.new(0, 100, 0, 150)
+				BillboardGui.StudsOffset = Vector3.new(0, 1, 0)
+				BillboardGui.AlwaysOnTop = true
+				TextLabel.Parent = BillboardGui
+				TextLabel.BackgroundTransparency = 1
+				TextLabel.Position = UDim2.new(0, 0, 0, -50)
+				TextLabel.Size = UDim2.new(0, 100, 0, 100)
+				TextLabel.Font = Enum.Font.SourceSansSemibold
+				TextLabel.TextSize = 20
+				TextLabel.TextColor3 = Color3.new(1, 1, 1)
+				TextLabel.TextStrokeTransparency = 0
+				TextLabel.TextYAlignment = Enum.TextYAlignment.Bottom
+				TextLabel.Text = 'Name: '..plr.Name
+				TextLabel.ZIndex = 10
+				local espLoopFunc
+				local teamChange
+				local addedFunc
+				addedFunc = plr.CharacterAdded:Connect(function()
+					if ESPenabled then
+						espLoopFunc:Disconnect()
+						teamChange:Disconnect()
+						ESPholder:Destroy()
+						repeat wait(1) until getRoot(plr.Character) and plr.Character:FindFirstChildOfClass("Humanoid")
+						ESP(plr)
+						addedFunc:Disconnect()
+					else
+						teamChange:Disconnect()
+						addedFunc:Disconnect()
+					end
+				end)
+				teamChange = plr:GetPropertyChangedSignal("TeamColor"):Connect(function()
+					if ESPenabled then
+						espLoopFunc:Disconnect()
+						addedFunc:Disconnect()
+						ESPholder:Destroy()
+						repeat wait(1) until getRoot(plr.Character) and plr.Character:FindFirstChildOfClass("Humanoid")
+						ESP(plr)
+						teamChange:Disconnect()
+					else
+						teamChange:Disconnect()
+					end
+				end)
+				local function espLoop()
+					if COREGUI:FindFirstChild(plr.Name..'_ESP') then
+						if plr.Character and getRoot(plr.Character) and plr.Character:FindFirstChildOfClass("Humanoid") and game.Players.LocalPlayer.Character and getRoot(game.Players.LocalPlayer.Character) and game.Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid") then
+							local pos = math.floor((getRoot(game.Players.LocalPlayer.Character).Position - getRoot(plr.Character).Position).magnitude)
+							if game.Players.LocalPlayer:IsFriendsWith(plr.UserId) then
+								TextLabel.Text = '[👤] Name: '..plr.Name..' | Health: '..round(plr.Character:FindFirstChildOfClass('Humanoid').Health, 1)..' | Studs: '..pos
+							else
+								TextLabel.Text = 'Name: '..plr.Name..' | Health: '..round(plr.Character:FindFirstChildOfClass('Humanoid').Health, 1)..' | Studs: '..pos
+							end
+						end
+					else
+						teamChange:Disconnect()
+						addedFunc:Disconnect()
+						espLoopFunc:Disconnect()
+					end
+				end
+				espLoopFunc = RunService.RenderStepped:Connect(espLoop)
+			end
+		end
+	end)
+end
+local PLAYERRBXESP
+Library.CreateButton(PlayerScript,"ESP","ESP (Name and Health)",function()
+	ESPenabled = not ESPenabled
+	if ESPenabled then
+		
+		coroutine.wrap(function()
+			PLAYERRBXESP = game.Players.PlayerAdded:Connect(function(play)
+				print('new player!')
+				play.CharacterAdded:Wait()
+				ESP(play)
+			end)
+		end)()
+		
+		for i,v in pairs(game.Players:GetPlayers()) do
+			if v.Name ~= game.Players.LocalPlayer.Name then
+				ESP(v)
+			end
+		end
+		
+	else
+		for i,c in pairs(COREGUI:GetChildren()) do
+			if string.sub(c.Name, -4) == '_ESP' then
+				c:Destroy()
+			end
+		end
+		
+		PLAYERRBXESP:Disconnect()
+	end
+end, 10000000000)
 
 local WSS = Library.CreateSection(GUI,"Simulators")
 
